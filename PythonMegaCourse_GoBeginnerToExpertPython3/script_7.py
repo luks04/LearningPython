70071978f0d1d7a34c0bcbfe2d8f7fa304bfd422
luks04 <67431911+luks04@users.noreply.github.com>
2021-09-10 00:40:05 -0500

    Arithmetic operators

---
+++ PythonMegaCourse_GoBeginnerToExpertPython3/script_7.py
@@ -0,0 +1,15 @@
+print (10//3) # -> It is an integer division
+print (5+3) # -> It is a normal addition
+print (10*3) # -> It is a normal multiplication
+#print 22*2 # -> It is missing the parentheses
+#Print (23/5) # -> It is not Capital letter at the beginning
+print (20/2) # -> It is a normal division which return a double value
+
+a = 200 # -> It is a normal assignment operation
+b = 3 # -> It is an other normal assignment operation
+c = 200 // 2 # -> It is an integer division (Arithmetic Operator) 
+c += b # -> It is incrementing the variable "c" by the value of "b" (Increment Assignment Operator)
+
+print(a)
+print(b)
+print(c)
