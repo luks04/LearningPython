5f7149dd8dbdf604d7756dcf10e308586c1fb333
luks04 <67431911+luks04@users.noreply.github.com>
2021-09-11 11:25:05 -0500

    Update Product.py

--- Python3_Curso_completo_de_cero_a_experto/classes/Product.py
+++ Python3_Curso_completo_de_cero_a_experto/classes/Product.py
@@ -1,2 +1,14 @@
 import random
 
+class Product:
+    def __init__(self, code: int, name: str, category: str, price: float) -> None:
+        self.code = code
+        self.name = name
+        self.category = category
+        self.price = price
+    
+    def get_stock(self):
+        return random.randint(0, 200)
+    
+    def __str__(self) -> str:
+        return f"\nCode: {self.code} \nName: {self.name} \nCategory: {self.category} \nPrice: {self.price}"
