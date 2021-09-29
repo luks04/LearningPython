0af15541a58145c7bc8c6031c7e858028aab8e6d
luks04 <67431911+luks04@users.noreply.github.com>
2021-09-29 01:07:48 -0500

    Improving assert statement example

--- python_language/script_4_assert_statement.py
+++ python_language/script_4_assert_statement.py
@@ -1,4 +1,7 @@
-var = 100
+try:
+    var = 100
 
-assert var == 100 # -> True
-assert var == 200 # -> False - Throw AssertionError
+    assert var == 100 # -> True
+    assert var == 200, 'Expected 200' # -> False - Throw AssertionError
+except AssertionError as error:
+    print(">>> Assertion Error: \n" + str(error))
