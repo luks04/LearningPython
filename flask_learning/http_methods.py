c0f579eefc04c0a9c537bd313eee6436af251486
luks04 <67431911+luks04@users.noreply.github.com>
2021-10-01 01:38:07 -0500

    Get headers

--- flask_learning/http_methods.py
+++ flask_learning/http_methods.py
@@ -5,6 +5,7 @@ http_methods = Blueprint('http_methods', __name__)
 @http_methods.route('/login', methods = ['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return "do_the_login()"
+        headers = request.headers
+        return f"do_the_login() with following headers: {headers}"
     else:
         return "show_the_login_form()"
