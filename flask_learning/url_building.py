c4008702ece105b247aeefa5fd40bce45df2f751
luks04 <67431911+luks04@users.noreply.github.com>
2021-10-01 01:17:03 -0500

    Create url_building.py
    Error handler
    Redirect
    Dynamic url builder

---
+++ flask_learning/url_building.py
@@ -0,0 +1,30 @@
+from flask import Blueprint, url_for, redirect, abort, request, jsonify
+from markupsafe import escape
+
+url_building = Blueprint('url_building', __name__)
+
+@url_building.route("/admin")
+def hello_admin():
+    return "Hello admin!"
+
+@url_building.route("/guest/<guest>")
+def hello_guest(guest: str):
+    return f"Hello {escape(guest)} as guest!"
+
+@url_building.route("/user/<name>")
+def hello_user(name: str):
+    if name == 'admin':
+        return redirect(url_for('url_building.hello_admin'))
+    elif name == 'hacker':
+        return not_found()
+    else:
+        return redirect(url_for('url_building.hello_guest', guest = name))
+
+@url_building.errorhandler(404)
+def not_found(error = None):
+    response = jsonify({
+        'message': 'Resource not found ' + request.url,
+        'status': 404,
+    })
+    response.status_code = 404
+    return response
