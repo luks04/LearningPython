9a543fcff77dd9f950a905265381a49e7615820c
luks04 <67431911+luks04@users.noreply.github.com>
2021-10-01 01:15:26 -0500

    Updating app
    Adding Blueprints
    Adding render template

--- flask_learning/app.py
+++ flask_learning/app.py
@@ -1,4 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from http_methods import http_methods
+from url_building import url_building
+from cookies import cookies
+from static_files import static_files
+
+app = Flask(__name__)
+app.register_blueprint(http_methods)
+app.register_blueprint(url_building)
+app.register_blueprint(cookies)
+app.register_blueprint(static_files)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/hello_world")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/hello/<name>/<city>/<int:id>")
+def hello(name: str, city: str, id: int):
+    return render_template("hello.html", name = name, city = city, id = id)
 
 if __name__ == '__main__':
-    print("Waiting...")
+    app.run(host = '0.0.0.0', port = 5000, debug = True)
